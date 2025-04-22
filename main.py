69a8a4fd24da3000f00f66e5eb377aecb17d2a08
Eric Bourcier-Walsh <fatkungfu@gmail.com>
2025-04-21 22:44:35 -0400

    add Drawing a Player

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 import pygame
-from constants import *  # noqa: F403
+from constants import *
+from player import Player
 
 
 def main():
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    running = True
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
-    while running:
+    while True:
         # poll for events
         # pygame.QUIT event means the user clicked X to close your window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                return
 
         # fill the screen with a color to wipe away anything from last frame
         screen.fill("black")
-
-        # RENDER YOUR GAME HERE
-
+        player.draw(screen)
         # flip() the display to put your work on screen
         pygame.display.flip()
-
-    pygame.quit()
+        # limit to 60 frames per second
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import *
+from circleshape import CircleShape
+
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def draw(self, screen):
+        # draw the player as a triangle
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        # draw the player as a circle
+        # pygame.draw.circle(screen, "white", (int(self.position.x), int(self.position.y)), self.radius)
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
